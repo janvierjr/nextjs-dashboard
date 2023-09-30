447fb22c1191dba2c0c318cfa38a7c4c9ee99eb7
janvierjr <bernie@myheartsleeve.com>
2023-09-30 12:15:28 -0500

    update sidebar toggle chevronflip and align flex-end

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,7 +4,6 @@ import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ListItem from '@mui/material/ListItem';
@@ -79,7 +78,7 @@ const Sidebar = () => {
       >
         <div className={`${scss.drawerHeader}`}>
           <IconButton onClick={handleDrawerToggle}>
-            {theme.direction === 'rtl' ? (
+            {!open ? (
               <ChevronRightIcon />
             ) : (
               <ChevronLeftIcon />
