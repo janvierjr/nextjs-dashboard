fa6de540c6e24ff65a8bb630e48ee6cb131123fa
janvierjr <bernie@myheartsleeve.com>
2023-10-05 10:57:50 -0500

    remove inter font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,14 @@
-import { Inter } from 'next/font/google'
+import React from 'react';
 import Dashboard from '@/pages/dashboard'
 import Login from '@/components/Login'
 import { useSession } from 'next-auth/react'
-import scss from '@/Home.module.scss'
-
-const inter = Inter({ subsets: ['latin'] })
+import scss from '@/Home.module.scss';
 
 const Home: React.FC = () => {
   const { data: session } = useSession();
 
   return (
-      <main className={`${inter.className} ${scss.main}`}>
+      <main className={`${scss.main}`}>
         {session && <Dashboard />}
         {!session && <Login />}
         <Login />
