b63662d858e4ee34998a4ddfa18639a832d08a65
janvierjr <bernie@myheartsleeve.com>
2023-10-06 15:21:20 -0500

    create TransactionsPerDay component AND scss AND update DataRibbon; ;

--- src/components/Dashboard/DataRibbon/DataRibbon.tsx
+++ src/components/Dashboard/DataRibbon/DataRibbon.tsx
@@ -1,37 +1,42 @@
 import React from 'react'
 import Grid from '@mui/material/Grid';
 import DataCard from '../DataCard';
+import TransactionPerDay from '../TransactionsPerDay/TransactionsPerDay';
+import scss from './DataRibbon.module.scss';
 
 const DataRibbon = () => {
   return (
-    <Grid container gap={2}>
-      <DataCard
-        title={'Total Sales'}
-        value={'564'}
-        description={'The totals of all DashData products in the current financial year'}
-        />
-      <Grid>
+    <>
+      <Grid container gap={3} className={scss.dataRibbon}>
         <DataCard
-        title={'Total Sales'}
-        value={'564'}
-        description={'The totals of all DashData products in the current financial year'}
-        />
+          title={'Total Sales'}
+          value={'564'}
+          description={'The totals of all DashData products in the current financial year'}
+          />
+        <Grid>
+          <DataCard
+          title={'Total Value'}
+          value={'$25,987.59'}
+          description={'Total sales of the current financial year'}
+          />
+        </Grid>
+        <Grid>
+          <DataCard
+          title={'Avg. Order Value'}
+          value={'$158.90'}
+          description={'The average order value of all sales this current financial year'}
+          />
+        </Grid>
+        <Grid>
+          <DataCard
+          title={'Conversion rate'}
+          value={'.59%'}
+          description={'Number of leads become sales'}
+          />
+        </Grid>
       </Grid>
-      <Grid>
-        <DataCard
-        title={'Total Sales'}
-        value={'564'}
-        description={'The totals of all DashData products in the current financial year'}
-        />
-      </Grid>
-      <Grid>
-        <DataCard
-        title={'Total Sales'}
-        value={'564'}
-        description={'The totals of all DashData products in the current financial year'}
-        />
-      </Grid>
-    </Grid>
+        <TransactionPerDay />
+    </>
   )
 }
 
---
+++ src/components/Dashboard/TransactionsPerDay/TransactionsPerDay.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import scss from './TransactionsPerDay.module.scss';
+import { Card, Grid, Paper } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/system';
+
+export type TransactionCardType = {
+  title: string;
+  value: string;
+  changeValue: string;
+}
+
+
+export type TransactionPerDayProps = {
+  data?: TransactionCardType;
+}
+
+const TransactionPerDay = (props: TransactionPerDayProps) => {
+  const { data } = props;
+  const theme = useTheme();
+
+  return (
+    <Grid container gap={3} className={scss.wrapper}>
+      <Paper className={scss.transactions}>
+        <div className={scss.chart}>
+          {/** CHART GOES HERE */}
+        </div>
+        <div className={scss.cardWrapper}>
+          <Card className={scss.card} variant={'outlined'}>
+            <div className={scss.cardTitle}>
+              <Typography>Total Products</Typography>
+            </div>
+            <div className={scss.cardValue}>
+              <Typography>1.275</Typography>
+              <Typography color={theme.palette.success.main} fontSize={14}> 428.7%</Typography>
+            </div>
+          </Card>
+          <Card className={scss.card} variant={'outlined'}>
+            <div className={scss.cardTitle}>
+              <Typography>Buy-to-details</Typography>
+            </div>
+            <div className={scss.cardValue}>
+              <Typography>4.49%</Typography>
+              <Typography color={theme.palette.success.main} fontSize={14}> 889.7%</Typography>
+            </div>
+          </Card>
+          <Card className={scss.card} variant={'outlined'}>
+            <div className={scss.cardTitle}>
+              <Typography>Refunds</Typography>
+            </div>
+            <div className={scss.cardValue}>
+              <Typography>15.6</Typography>
+              <Typography color={theme.palette.success.main} fontSize={14}> 3.7%</Typography>
+            </div>
+          </Card>
+        </div>
+      </Paper>
+    </Grid>
+  )
+}
+
+export default TransactionPerDay;
