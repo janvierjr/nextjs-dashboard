fd51b32a763df092c30f7ada92ef3bedb7285a21
janvierjr <bernie@myheartsleeve.com>
2023-10-13 23:12:18 -0500

    fix login button delay on dashboard load

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useSession, signIn, signOut } from 'next-auth/react';
 import Button from '@mui/material/Button';
 
 const Login = () => {
   const { data: session } = useSession();
+  const [buttonAppear, setButtonAppear] = useState(false);
 
-  if (session) {
+// delay login button to let data dashboard load
+  const DelayButtonAppearance = () => {
+    return setTimeout(() => (
+      setButtonAppear(true)
+    ), 400);
+  }
+  useEffect(() => {
+    DelayButtonAppearance()
+  }, [])
+
+  if (!session) {
     return (
+      <>
+        {buttonAppear &&
+          <Button variant='contained' color='success' onClick={() => signIn()}>
+            Login
+          </Button>
+        }
+      </>
+    );
+  }
+  return (
       <>
         <Button variant='contained' color='error' onClick={() => signOut()}>
           Sign out
         </Button>
         <p>Signed in as {session?.user?.email}</p>
       </>
-    );
-  }
-  return (
-    <>
-      <Button variant='contained' color='success' onClick={() => signIn()}>
-        Login
-      </Button>
-    </>
   )
 };
 
